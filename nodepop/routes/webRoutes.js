39b52352af785833b7895b3244a837382ab6f176
MarianDev22 <marianmfalicante@gmail.com>
2025-11-09 20:11:52 +0100

    feat: pagination logic

--- nodepop/routes/webRoutes.js
+++ nodepop/routes/webRoutes.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import { query, param } from 'express-validator';
 
 //Models
 import { Product } from '../models/Product.js';
@@ -21,8 +22,38 @@ router.get('/', guard, (req, res, next) => {
     res.render('home.html');
 });
 
-router.get('/products', guard, productController.getAll);
+router.get('/products', 
+        query('skip', 'Must be a valid positive number')
+        .optional()
+        .isInt({
+            min: 1,
+        })
+        .toInt(),
+    query('limit', 'Must be a valid positive number')
+        .optional()
+        .isInt({
+            min: 1
+        })
+        .toInt(),
+    query('minPrice', 'Must be a valid positive number')
+        .optional()
+        .isInt({ min: 1 })
+        .toInt(),
+    query('maxPrice', 'Must be a valid positive number')
+        .optional()
+        .isInt({ min: 1 })
+        .toInt(),
+    query('name', 'Must be a valid name')
+        .optional()
+        .isString(),
+    query('name', 'Must be a valid tag')
+        .optional()
+        .isIn(['work', 'lifestyle', 'motor', 'mobile']),
+        guard, productController.getAll);
 
+router.get('/addProduct',productController.add);
+
+//Login
 router.get('/login', loginController.index);
 router.post('/login', loginController.login);
 router.get('/logout', loginController.logout);
